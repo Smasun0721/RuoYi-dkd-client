24056616d8a34b094ed3f75dd5ec346f30edaad0
Smasun <3391470282@qq.com>
2026-05-15 18:16:26 +0800

    货道管理

--- src/api/manage/channel.js
+++ src/api/manage/channel.js
@@ -1,44 +1,67 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 查询售货机货道列表
 export function listChannel(query) {
   return request({
-    url: '/manage/channel/list',
-    method: 'get',
-    params: query
-  })
+    url: "/manage/channel/list",
+    method: "get",
+    params: query,
+  });
 }
 
 // 查询售货机货道详细
 export function getChannel(id) {
   return request({
-    url: '/manage/channel/' + id,
-    method: 'get'
-  })
+    url: "/manage/channel/" + id,
+    method: "get",
+  });
 }
 
 // 新增售货机货道
 export function addChannel(data) {
   return request({
-    url: '/manage/channel',
-    method: 'post',
-    data: data
-  })
+    url: "/manage/channel",
+    method: "post",
+    data: data,
+  });
 }
 
 // 修改售货机货道
 export function updateChannel(data) {
   return request({
-    url: '/manage/channel',
-    method: 'put',
-    data: data
-  })
+    url: "/manage/channel",
+    method: "put",
+    data: data,
+  });
 }
 
 // 删除售货机货道
 export function delChannel(id) {
   return request({
-    url: '/manage/channel/' + id,
-    method: 'delete'
-  })
+    url: "/manage/channel/" + id,
+    method: "delete",
+  });
+}
+
+// 查询货道列表
+export function getGoodsList(innerCode) {
+  return request({
+    url: "/manage/channel/list/" + innerCode,
+    method: "get",
+  });
+}
+// 查询设备类型
+export function getGoodsType(typeId) {
+  return request({
+    url: "/manage/vmType/" + typeId,
+    method: "get",
+  });
+}
+// 提交获取的货道
+export function channelConfig(data) {
+  return request({
+    url: "/manage/channel/config",
+    method: "put",
+    data: data,
+  });
 }
